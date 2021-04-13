a1180b5ee8265e8f7f1e672ef121a7a7e7333147
Jules Scheil <juliana.m.scheil@gmail.com>
2021-04-13 19:01:40 -0400

    added constructors for employee and manager

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const fs = require("fs");
-const OUTPUT_DIR = path.resolve(__dirname, "dist")
+const employee = require("./lib/Employee")
+const pageTemplate = require("./src/page-template");
+const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 function runApp() {
-  ...Inquirer prompt and the functions that will ask users about manager, intern, and engineer.
+//   ...Inquirer prompt and the functions that will ask users about manager, intern, and engineer.
   function buildTeam() {
     // Create the output directory if the output path doesn't exist
     if (!fs.existsSync(OUTPUT_DIR)) {
