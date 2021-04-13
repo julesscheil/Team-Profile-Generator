7563f5f9edfdb5dc77ee3ee0ebad9f9868982286
Jules Scheil <juliana.m.scheil@gmail.com>
2021-04-13 19:41:30 -0400

    added starter template

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const path = require("path");
 const fs = require("fs");
-const employee = require("./lib/Employee")
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const pageTemplate = require("./src/page-template");
 const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+
+
+
+
 
 function runApp() {
 //   ...Inquirer prompt and the functions that will ask users about manager, intern, and engineer.
--- src/page-template.js
+++ src/page-template.js
@@ -1,18 +1,46 @@
 const generateTeam = team => {
-    const  generateManager = manager = {
+
+    // create manager html
+    const generateManager = manager => {
         // ` return ${manager.getName()} etc etc`
-    }
+        return `
+        <div> <h1>Manager Stuff</h1></div>
+        <h2>${manager.getName()}</h2>`;
+    };
+    // create manager html
+    const generateEngineer = engineer => {
+        // ` return ${manager.getName()} etc etc`
+        return `
+    <div> <h1>Engineer Stuff</h1></div>
+    <h2>${engineer.getName()}</h2>`;
+    };
+    // create manager html
+    const generateIntern = intern => {
+        // ` return ${manager.getName()} etc etc`
+        return `
+    <div> <h1>Manager Stuff</h1></div>
+    <h2>${intern.getName()}</h2>`;
+    };
+
     const html = [];
-        html.push(team
-            .filter(employee => employee.getRole() === "Manager")
-            .map(manager => generateManager(manager))
-        );
-        return html.join("");
-    }
-    module.exports = team => {
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+    );
+    return html.join("");
+}
+
+
+
+
+module.exports = team => {
     //   HTML TEMPLATE
     //       WHERE YOU WANT TO SHOW THE USER CONTENT
     //     <div>
     //       ${generateTeam(team)}
     //     </div>
-    }
+}
