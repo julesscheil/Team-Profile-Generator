580b6db0c748b89b8a76973662c8728c1aec06cf
Jules Scheil <juliana.m.scheil@gmail.com>
2021-04-13 23:06:17 -0400

    added engineer tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,31 @@
+const Engineer =require('../lib/Engineer');
+
+describe("tests if sub class exists", () => {
+    it('should be an object', () => {
+        //arrange
+        //act
+        const engineerObject = new Engineer(); 
+        //assert 
+        expect(typeof(engineerObject)).toBe('object');
+    })
+    it('should take in a name', () => {
+        const name = "jules"
+        const engineerObject = new Engineer("jules");
+        expect(engineerObject.name).toBe(name);
+    })
+    it('should take in an id', () => {
+        const id = 1
+        const engineerObject = new Engineer("jules",1);
+        expect(engineerObject.id).toBe(id);
+    })
+    it('should take in an email', () => {
+        const email = "jules@gmail.com"
+        const engineerObject = new Engineer("jules", 1,"jules@gmail.com");
+        expect(engineerObject.email).toBe(email);
+    })
+    it('should take in a GitHub username', () => {
+        const gitHub = "julesscheil"
+        const engineerObject = new Engineer("jules", 1,"jules@gmail.com","julesscheil");
+        expect(engineerObject.gitHub).toBe(gitHub);
+    })
+})
