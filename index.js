dca20b4d08e379ab19fa23a12b912fe0f9255e18
Jules Scheil <juliana.m.scheil@gmail.com>
2021-04-13 20:31:43 -0400

    added inquirer manager prompt

--- index.js
+++ index.js
@@ -9,18 +9,44 @@ const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 
-
-
-
-
 function runApp() {
-//   ...Inquirer prompt and the functions that will ask users about manager, intern, and engineer.
-  function buildTeam() {
-    // Create the output directory if the output path doesn't exist
-    if (!fs.existsSync(OUTPUT_DIR)) {
-      fs.mkdirSync(OUTPUT_DIR)
+  //   ...Inquirer prompt and the functions that will ask users about manager, intern, and engineer.
+  inquirer.prompt([
+    // console.log("Please give your team's information:") ,
+    {
+      type: 'input',
+      name: 'managerName',
+      message: "What is your manager's name?",
+    },
+    {
+      type: 'input',
+      name: 'managerID',
+      message: "What is the manager's ID?",
+    },
+    {
+      type: 'input',
+      name: 'managerEmail',
+      message: "What is the manager's email?",
+    },
+    {
+      type: 'input',
+      name: 'managerOfficeNumber',
+      message: "What is the manager's office number?"
     }
-    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
-  }
+  ]).then(answers => {
+    
+    console.log(answers);
+  })
+
 }
+
+
+//   function buildTeam() {
+//     // Create the output directory if the output path doesn't exist
+//     if (!fs.existsSync(OUTPUT_DIR)) {
+//       fs.mkdirSync(OUTPUT_DIR)
+//     }
+//     fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+//   }
+// }
 runApp();
